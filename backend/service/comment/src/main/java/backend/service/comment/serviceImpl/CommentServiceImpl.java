1ce1e450ec1677aab774e262ac111cd3d71b61ef
JayJi <jayji@JayJiui-MacBookPro.local>
2025-10-19 20:46:34 +0900

    [REFACTOR] 댓글 무한 Depth 기능으로 변경

--- backend/service/comment/src/main/java/backend/service/comment/serviceImpl/CommentServiceImpl.java
+++ backend/service/comment/src/main/java/backend/service/comment/serviceImpl/CommentServiceImpl.java
@@ -1,14 +1,19 @@
 package backend.service.comment.serviceImpl;
 
+import backend.service.comment.common.PageLimitCalculator;
 import backend.service.comment.dto.request.CommentCreateRequestDto;
+import backend.service.comment.dto.response.CommentPageResponse;
 import backend.service.comment.dto.response.CommentResponseDto;
 import backend.service.comment.entity.CommentEntity;
+import backend.service.comment.entity.CommentPath;
 import backend.service.comment.repository.CommentRepository;
 import backend.service.comment.service.CommentService;
 import jakarta.transaction.Transactional;
 import kuke.board.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static java.util.function.Predicate.not;
 
@@ -20,20 +25,31 @@ public class CommentServiceImpl implements CommentService {
     private final Snowflake snowflake = new Snowflake();
 
     @Transactional
-    public CommentResponseDto create(CommentCreateRequestDto commentCreateRequestDto) {
-        CommentEntity commentEntity = findParent(commentCreateRequestDto);
-        CommentEntity comment = commentRepository.save(CommentEntity.create(snowflake.nextId(), commentCreateRequestDto.getContent(), commentEntity == null ? null : commentEntity.getParentCommentId(), commentCreateRequestDto.getUserId(), commentCreateRequestDto.getBoardId()));
+    public CommentResponseDto create(CommentCreateRequestDto requestDto) {
+        CommentEntity parent = findParent(requestDto);
+        CommentPath parentCommentPath = parent == null ? CommentPath.create("") : parent.getCommentPath();
+
+        CommentEntity comment = commentRepository.save(
+                CommentEntity.create(
+                        snowflake.nextId(),
+                        requestDto.getContent(),
+                        parentCommentPath.createChildCommentPath(
+                                commentRepository.findDescendantsTopPath(
+                                        requestDto.getBoardId(), parentCommentPath.getPath()).orElse(null)),
+                        requestDto.getUserId(),
+                        requestDto.getBoardId()));
         return CommentResponseDto.from(comment);
     }
 
-    private CommentEntity findParent(CommentCreateRequestDto commentCreateRequestDto) {
-        Long parentCommentId = commentCreateRequestDto.getParentCommentId();
-        if (parentCommentId == null) {
+    private CommentEntity findParent(CommentCreateRequestDto requestDto) {
+        String parentPath = requestDto.getParentPath();
+        if (parentPath == null) {
             return null;
         }
-        return commentRepository.findById(parentCommentId).filter(not(CommentEntity::getIsDelete)).filter(CommentEntity::isRoot).orElseThrow();
+        return commentRepository.findByPath(parentPath)
+                .filter(not(CommentEntity::getIsDelete))
+                .orElseThrow();
     }
-
 
     public CommentResponseDto read(Long commentId) {
         return CommentResponseDto.from(commentRepository.findById(commentId).orElseThrow());
@@ -52,13 +68,36 @@ public class CommentServiceImpl implements CommentService {
     }
 
     private boolean hasChildren(CommentEntity commentEntity) {
-        return commentRepository.countBy(commentEntity.getBoardId(), commentEntity.getParentCommentId(), 2L) == 2L;
+        return commentRepository.findDescendantsTopPath(commentEntity.getBoardId(), commentEntity.getCommentPath().getPath()).isPresent();
     }
 
     private void delete(CommentEntity commentEntity) {
         commentRepository.delete(commentEntity);
         if (!commentEntity.isRoot()) {
-            commentRepository.findById(commentEntity.getCommentId()).filter(CommentEntity::getIsDelete).filter(not(this::hasChildren)).ifPresent(this::delete);
+            commentRepository.findByPath(commentEntity.getCommentPath().getParentPath())
+                    .filter(CommentEntity::getIsDelete)
+                    .filter(not(this::hasChildren))
+                    .ifPresent(this::delete);
         }
     }
+
+    public CommentPageResponse readAll(Long boardId, Long page, Long pageSize) {
+        return CommentPageResponse.of(
+                commentRepository.findAll(boardId, (page - 1) * pageSize, pageSize).stream()
+                        .map(CommentResponseDto::from)
+                        .toList(),
+                commentRepository.count(boardId, PageLimitCalculator.calculatePageLimit(page, pageSize, 10L))
+        );
+    }
+
+    public List<CommentResponseDto> readAllInfiniteScroll(Long boardId, String lastPath, Long pageSize) {
+        List<CommentEntity> comments = lastPath == null ?
+                commentRepository.findAllInfiniteScroll(boardId, pageSize) :
+                commentRepository.findAllInfiniteScroll(boardId, lastPath, pageSize);
+
+        return comments.stream()
+                .map(CommentResponseDto::from)
+                .toList();
+    }
+
 }
