b5b1019c7c1cb6f648bbc4fa00d451a5b2a62161
jayji5204 <jayji5204@naver.com>
2025-12-08 16:08:34 +0900

    [ADD]: JwtFilter 적용

--- backend/service/user/src/main/java/backend/service/user/security/SecurityConfig.java
+++ backend/service/user/src/main/java/backend/service/user/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package backend.service.user.security;
 
+import backend.service.user.jwt.JwtFilter;
 import backend.service.user.jwt.JwtUtil;
 import backend.service.user.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +28,7 @@ public class SecurityConfig {
 
     public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtUtil jwtUtil) {
         this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtUtil=jwtUtil;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
@@ -38,13 +44,34 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setMaxAge(3600L);
+                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                return configuration;
+            }
+        }));
         http.csrf((auth) -> auth.disable());
         http.formLogin((auth) -> auth.disable());
         http.httpBasic((auth) -> auth.disable());
 
-        http.authorizeHttpRequests((auth) -> auth.requestMatchers("/", "/api/**").permitAll().requestMatchers("/admin").hasRole("ADMIN").anyRequest().authenticated());
+        http.authorizeHttpRequests((auth) -> auth.
+                requestMatchers("/", "/api/**").permitAll().
+                requestMatchers("/api/auth/admin").hasRole("ADMIN").
+                anyRequest().
+                authenticated());
 
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
