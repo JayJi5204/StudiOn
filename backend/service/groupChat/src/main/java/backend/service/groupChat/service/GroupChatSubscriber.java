156de4ccaf97c3b8fecdfc235309dc593898e3c7
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-04-12 02:58:58 +0900

    [FIX]: nginx location추가

---
+++ backend/service/groupChat/src/main/java/backend/service/groupChat/service/GroupChatSubscriber.java
@@ -0,0 +1,37 @@
+package backend.service.chat.service;
+
+import backend.service.chat.dto.response.CreateResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ChatSubscriber implements MessageListener {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            // Redis에서 받은 메시지 역직렬화
+            String body = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
+            CreateResponse chatMessage = objectMapper.readValue(body, CreateResponse.class);
+            log.info("Redis 수신 body={}", body);  // 추가
+            // WebSocket으로 구독자들에게 전달
+            messagingTemplate.convertAndSend("/sub/chat/" + chatMessage.getRoomId(), chatMessage);
+
+            log.info("메시지 전달 roomId={}", chatMessage.getRoomId());
+        } catch (Exception e) {
+            log.error("메시지 처리 오류", e);
+        }
+    }
+}
